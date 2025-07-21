c9e083e2a650a077f13566cb31e67488d108026c
Udit <uditsaurabh747@gmail.com>
2025-07-21 23:05:02 +0530

    Adding further code

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { join } from 'path';
-import * as express from 'express';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { Request, Response, NextFunction } from 'express'; // 👈 Add this
+import { join } from 'path';
+import { Request, Response } from 'express';
+
+
+
+console.log('Starting app...');
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('DATABASE_URL:', process.env.DATABASE_URL ? '✓ present' : '⛔ missing');
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Serve static frontend
-  app.use(express.static(join(__dirname, '..', 'frontend', 'dist')));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  // Add types to middleware
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    if (!req.path.startsWith('/api')) {
-      res.sendFile(join(__dirname, '..', 'frontend', 'dist', 'index.html'));
-    } else {
-      next();
-    }
+  app.use((req: Request, res: Response) => {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
   });
 
   await app.listen(process.env.PORT || 3000);
